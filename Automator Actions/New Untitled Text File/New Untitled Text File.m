61585248a13f88fce15228da339d80fc8e1b4c9b
kolpanic <kolpanic@0ca8f281-d12c-0410-a1d3-69393e245e82>
2007-04-11 03:32:32 +0000

    Fix comment line breaks so they show up on the projector

--- Automator Actions/New Untitled Text File/New Untitled Text File.m
+++ Automator Actions/New Untitled Text File/New Untitled Text File.m
@@ -13,15 +13,16 @@
 - (id)runWithInput:(id)input fromAction:(AMAction *)anAction error:(NSDictionary **)errorInfo {
 	
 	// First, set the contents of the file depending on the option chosen in Automator.	
-	// The key initialContentsOption is in the Parameters NSObjectController instantiated in the Nib
-	// and then bound (via Cocoa bindings) to the NSPopUpButton's selected index.
+	// The key initialContentsOption is in the Parameters NSObjectController instantiated in
+	// the Nib and then bound (via Cocoa bindings) to the NSPopUpButton's selected index.
 	// It's also an integer parameter in the Target's properties (Parameters collection)
 	
 	NSString *initialContents = @"";
 	if ([[[self parameters] objectForKey:@"initialContentsOption"] intValue] == 1)
 		initialContents = LOREM_IPSUM;
 	
-	// Use the action's input as the location to create the new file. If it's not set, use ~/Desktop/.
+	// Use the action's input as the location to create the new file.
+	//If it's not set, use ~/Desktop/.
 	NSString *path = nil;
 	if ([input isKindOfClass:[NSArray class]])
 		path = [input objectAtIndex:0];
@@ -30,11 +31,13 @@
 	if (!path || [path isEqualToString:@""])
 		path = [@"~/Desktop" stringByExpandingTildeInPath];
 	
-	// Set the file name to Untitled.txt. If it already exists at the path, append an index number.
+	// Set the file name to Untitled.txt. If it already exists at the path, 
+	//append an index number.
 	NSString *newFile = [path stringByAppendingPathComponent:@"Untitled.txt"];
 	int index = 1;
 	for (index; [[NSFileManager defaultManager] fileExistsAtPath:newFile]; index++)
-		newFile = [path stringByAppendingPathComponent:[NSString stringWithFormat:@"Untitled %d.txt", index]];
+		newFile = [path stringByAppendingPathComponent:
+			[NSString stringWithFormat:@"Untitled %d.txt", index]];
 	
 	// Create the file.
 	[initialContents writeToFile:newFile atomically:NO encoding:NSUTF8StringEncoding error:nil];
